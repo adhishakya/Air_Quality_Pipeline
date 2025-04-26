4d8479a3e7dba409320c53dffc86f0472c473801
adhishakya <shakyaadhish@gmail.com>
2025-04-27 00:12:01 +0545

    api request logic

---
+++ utils/api_request.py
@@ -0,0 +1,12 @@
+import requests
+import logging
+
+def fetch_data_from_api(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status() 
+        json_response = response.json()
+        return json_response
+    except Exception as e:
+        logging.error(f"Error fetching data from API: {e}")
+        return None
