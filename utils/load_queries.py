a92d43b9ee7ac0bd32cd87e5305480e947244d51
adhishakya <shakyaadhish@gmail.com>
2025-05-11 01:36:27 +0545

    path independency

--- utils/load_queries.py
+++ utils/load_queries.py
@@ -1,13 +1,19 @@
+import os
 import logging
 
 logging.basicConfig(level = logging.INFO)
 def load_queries():
     try:
-        with open('sql/create_table.sql', 'r') as file:
-            create_table_query = file.read()
+        root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        sql_dir = os.path.join(root_dir, 'sql')
 
-        with open('sql/insert_data.sql', 'r') as file:
+        with open(os.path.join(sql_dir,'create_table.sql'), 'r') as file:
+            create_table_query = file.read()
+            print(create_table_query)
+
+        with open(os.path.join(sql_dir,'insert_data.sql'), 'r') as file:
             insert_data_query = file.read()
+            print(insert_data_query)
         
         return create_table_query, insert_data_query
     
